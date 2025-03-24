28b0192e08dcee875e91db9dd3dcedf815f0a8a8
3raphat <96657413+3raphat@users.noreply.github.com>
2025-03-24 16:30:09 +0700

    feat: pettier

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+const config = {
+  bracketSpacing: true,
+  semi: false,
+  trailingComma: "all",
+  printWidth: 80,
+  tabWidth: 2,
+  singleQuote: false,
+};
+
+export default config;
